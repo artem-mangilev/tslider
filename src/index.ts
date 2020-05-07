8c253032b056805707d95b889714d99dd967bbf5
Artem <artmnglv@gmail.com>
2020-05-07 18:43:34 +0500

    create NumRange and Handle classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import NumRange from './NumRange'
+
+const range = new NumRange(40, 60)
+console.log(range.getNumber(50))
+
+import Handle from './Handle'
+
+const handle = new Handle(10, 200, 50);
+console.log(handle.position)
+
 
