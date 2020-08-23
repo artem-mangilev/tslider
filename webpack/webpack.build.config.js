9c41736eb02f96bdfee2dcff5d2779d1a3cb28d6
Artem <artmnglv@gmail.com>
2020-08-23 17:16:50 +0500

    add code to build part of config that allows to show app correctly in gh pages

--- webpack/webpack.build.config.js
+++ webpack/webpack.build.config.js
@@ -1,8 +1,16 @@
 const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.config')
 
+const GH_PAGES_PUBLIC_PATH = '/tslider/'
+
 const buildWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
+
+  output: {
+    filename: '[name].js',
+    path: baseWebpackConfig.externals.paths.build,
+    publicPath: GH_PAGES_PUBLIC_PATH,
+  },
 })
 
 module.exports = new Promise((resolve, reject) => {
