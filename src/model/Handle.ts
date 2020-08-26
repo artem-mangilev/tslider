42c3a7171851480b954a6cc37bc75d5322887acf
Artem <artmnglv@gmail.com>
2020-08-26 14:57:42 +0500

    remove unnecessary code from handle

--- src/model/Handle.ts
+++ src/model/Handle.ts
@@ -1,22 +1,5 @@
-import { OneDimensionalSpacePoint } from '../utils/aliases'
-
 class Handle {
-  private currentPosition: OneDimensionalSpacePoint
-
-  constructor(initialCoord: OneDimensionalSpacePoint) {
-    // when this class is initialized, it assumes that
-    // passed coordinate is for horizontal orientation,
-    // so Y uses as a passive axis which isn't changes
-    this.currentPosition = initialCoord
-  }
-
-  public get position(): OneDimensionalSpacePoint {
-    return this.currentPosition
-  }
-
-  public set position(newPosition) {
-    this.currentPosition = newPosition
-  }
+  constructor(public position: number) {}
 }
 
 export default Handle
