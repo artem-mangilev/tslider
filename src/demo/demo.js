b518308fb9c6b867e03e167ef6d47dca1b5f0cbf
Artem <artmnglv@gmail.com>
2020-09-14 14:29:46 +0500

    create demo page

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -2,7 +2,7 @@ import './demo.scss'
 
 import '../tsliderPlugin'
 
-let $plugin1 = $('.tsl-target-1').tsliderPlugin({
+$('.slider_horizontal .slider__target').tsliderPlugin({
   min: 0,
   max: 100,
   from: 0,
@@ -16,21 +16,7 @@ let $plugin1 = $('.tsl-target-1').tsliderPlugin({
   orientation: 'horizontal',
 })
 
-$plugin1 = $('.tsl-target-1').tsliderPlugin({
-  min: 0,
-  max: 100,
-  from: 0,
-  to: 50,
-  step: 10,
-  showRuler: true,
-  rulerSteps: 4,
-  isRulerClickable: true,
-  showLabels: true,
-  inputValuesSeparator: ', ',
-  orientation: 'horizontal',
-})
-
-const $plugin2 = $('.tsl-target-2').tsliderPlugin({
+$('.slider_vertical .slider__target').tsliderPlugin({
   min: 0,
   max: 100,
   from: 10,
@@ -41,17 +27,52 @@ const $plugin2 = $('.tsl-target-2').tsliderPlugin({
   isRulerClickable: true,
   showLabels: true,
   inputValuesSeparator: ', ',
-  orientation: 'horizontal',
+  orientation: 'vertical',
 })
 
-$plugin1.tsliderPlugin('setTo', 80)
+const $blocks = $('.slider')
 
-$plugin2.tsliderPlugin('setFrom', 40)
+$blocks.each(function () {
+  const $block = $(this)
 
-console.log($plugin2.tsliderPlugin('getFrom'))
+  const $from = $block.find('.slider__from-value')
+  const $to = $block.find('.slider__to-value')
 
-$plugin2.tsliderPlugin('hideRuler')
-// $plugin2.tsliderPlugin('hideLabels')
-$plugin2.tsliderPlugin('onUpdate', (value) => {
-  console.log(value)
+  const $plugin = $block.find('.slider__target')
+
+  $from.val($plugin.tsliderPlugin('getFrom'))
+  $to.val($plugin.tsliderPlugin('getTo'))
+
+  $plugin.tsliderPlugin('onUpdate', () => {
+    $from.val($plugin.tsliderPlugin('getFrom'))
+    $to.val($plugin.tsliderPlugin('getTo'))
+  })
+
+  $from.on('focusout', () => {
+    $plugin.tsliderPlugin('setFrom', $from.val())
+  })
+
+  $to.on('focusout', () => {
+    $plugin.tsliderPlugin('setTo', $to.val())
+  })
+
+  const $showLabels = $block.find('.slider__show-labels')
+
+  $showLabels.on('change', function () {
+    if (this.checked) {
+      $plugin.tsliderPlugin('showLabels')
+    } else {
+      $plugin.tsliderPlugin('hideLabels')
+    }
+  })
+
+  const $showRuler = $block.find('.slider__show-ruler')
+
+  $showRuler.on('change', function () {
+    if (this.checked) {
+      $plugin.tsliderPlugin('showRuler')
+    } else {
+      $plugin.tsliderPlugin('hideRuler')
+    }
+  })
 })
