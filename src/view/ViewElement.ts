d14d31e59c6a59cd19d4492014327bbc0588bd14
Artem <artmnglv@gmail.com>
2020-09-26 15:39:00 +0500

    create HTMLViewElement class

--- src/view/ViewElement.ts
+++ src/view/ViewElement.ts
@@ -1,5 +1,9 @@
 import Point from '../utils/Point'
 
+export interface RenderPermitter {
+  shouldRerender(currentState: any, newState: any): boolean
+}
+
 // TODO: maybe split this to several interfaces?
 export interface ViewElement {
   width: number
@@ -7,7 +11,7 @@ export interface ViewElement {
   position: Point
   move(point: Point): void
 
-  add(...elements: ViewElement[]): ViewElement
+  add(...elements: ViewElement[]): void
   after(element: ViewElement): void
 
   setContent(content: string): void
@@ -17,9 +21,85 @@ export interface ViewElement {
   hide(): void
 
   setAttribute(name: string, value: string): void
-
-  shouldRerender(currentState: any, newState: any): boolean
 }
+
+class HTMLViewElement implements ViewElement {
+  private $elem: JQuery<HTMLElement>
+
+  constructor(element: string | HTMLElement, className?: string) {
+    if (element instanceof HTMLElement) {
+      this.$elem = $(element)
+    }
+
+    if (typeof element === 'string') {
+      this.$elem = $(`<${element}>`, {
+        class: className,
+      })
+    }
+  }
+
+  get width(): number {
+    return this.$elem.outerWidth()
+  }
+
+  get height(): number {
+    return this.$elem.outerHeight()
+  }
+
+  set width(newWidth: number) {
+    this.$elem.css('width', `${newWidth}px`)
+  }
+
+  set height(newHeight: number) {
+    this.$elem.css('height', `${newHeight}px`)
+  }
+
+  get position(): Point {
+    const { x, y } = this.$elem[0].getBoundingClientRect()
+
+    return { x, y }
+  }
+
+  move(position: Point): void {
+    this.$elem.css('transform', `translate(${position.x}px, ${position.y}px)`)
+  }
+
+  add(...elements: ViewElement[]): void {
+    elements.forEach((element) => {
+      if (element instanceof HTMLViewElement) {
+        this.$elem.append(element.$elem)
+      }
+    })
+  }
+
+  after(element: ViewElement): void {
+    if (element instanceof HTMLViewElement) {
+      this.$elem.after(element.$elem)
+    }
+  }
+
+  setContent(content: string): void {
+    this.$elem.html(content)
+  }
+
+  getContent(): string {
+    return this.$elem.html()
+  }
+
+  show(): void {
+    this.$elem.css('position', 'static')
+  }
+
+  hide(): void {
+    this.$elem.css('position', 'absolute').css('left', '-999px')
+  }
+
+  setAttribute(name: string, value: string): void {
+    this.$elem.attr(name, value)
+  }
+}
+
+export default HTMLViewElement
 
 export interface ViewElementEvents {
   listen(...elements: ViewElement[]): void
