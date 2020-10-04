e469ed7e4b79ed0ab4a3b908ccb4b3444a612b87
Artem <artmnglv@gmail.com>
2020-10-04 17:34:10 +0500

    simplify throttle implementation

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,22 +1,15 @@
-/* eslint-disable @typescript-eslint/no-this-alias */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-type VariadicCallback = (...args: any[]) => void
+type VariadicCallback = (...args: unknown[]) => void
 export const throttle = (fn: VariadicCallback, wait: number) => {
-  let inThrottle: boolean, lastFn: any, lastTime: number
-  return function (...args: any[]) {
-    const context = this
-    if (!inThrottle) {
-      fn.apply(context, args)
-      lastTime = Date.now()
-      inThrottle = true
-    } else {
-      clearTimeout(lastFn)
-      lastFn = setTimeout(function () {
-        if (Date.now() - lastTime >= wait) {
-          fn.apply(context, args)
-          lastTime = Date.now()
-        }
-      }, Math.max(wait - (Date.now() - lastTime), 0))
+  let waiting = false
+  return (...args: unknown[]) => {
+    if (!waiting) {
+      fn.apply(this, args)
+
+      waiting = true
+
+      setTimeout(() => {
+        waiting = false
+      }, wait)
     }
   }
 }
