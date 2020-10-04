8922a57b8f6f063af76415df8f8272ec078f1c8a
Artem <artmnglv@gmail.com>
2020-10-04 11:50:03 +0500

    RenderPermitter: move equals to this class

--- src/utils/RenderPermitter.ts
+++ src/utils/RenderPermitter.ts
@@ -1,16 +1,40 @@
-import { equals } from './equals'
-
 export interface RenderPermitter {
-  shouldRerender(data: any): boolean
+  shouldRerender(data: unknown): boolean
 }
 
 class RenderStatePermitter implements RenderPermitter {
   private state: unknown
 
   shouldRerender(state: unknown): boolean {
-    const shouldRerender = !equals(this.state, state)
+    const shouldRerender = !this.equals(this.state, state)
     this.state = state
     return shouldRerender
+  }
+
+  /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+  private equals(a: any, b: any): boolean {
+    if (a === b) {
+      return true
+    }
+
+    if (a instanceof Date && b instanceof Date) {
+      return a.getTime() === b.getTime()
+    }
+
+    if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')) {
+      return a === b
+    }
+
+    if (a.prototype !== b.prototype) {
+      return false
+    }
+
+    const keys = Object.keys(a)
+    if (keys.length !== Object.keys(b).length) {
+      return false
+    }
+
+    return keys.every((k) => this.equals(a[k], b[k]))
   }
 }
 
